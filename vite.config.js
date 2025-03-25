c2244394bbb2ea22e27e23ac72d9c26e905d8550
Oluwatobi97 <victortobi2000@gamil.com>
2025-03-25 20:11:12 +0100

    make some changes

--- vite.config.js
+++ vite.config.js
@@ -9,14 +9,6 @@ export default defineConfig({
     outDir: "dist",
     assetsDir: "assets",
     sourcemap: true,
-    minify: "terser",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom", "react-router-dom"],
-        },
-      },
-    },
   },
   server: {
     port: 3000,
